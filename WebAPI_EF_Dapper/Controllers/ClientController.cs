a8b19399ad9e4b9dc1b854a50d41f5d025800f40
Komron <komronabdujabbor@gmail.com>
2023-09-06 18:47:40 +0500

    added WebApi

---
+++ WebAPI_EF_Dapper/Controllers/ClientController.cs
@@ -0,0 +1,47 @@
+﻿global using DapperAndEFcore.Models;
+using DapperAndEFcore.Service.DapperFolder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_EF_Dapper.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ClientServiceDapper clientServiceDapper;
+
+        public ClientController(ClientServiceDapper clientServiceDapper)
+        {
+            this.clientServiceDapper = clientServiceDapper;
+        }
+        [HttpGet]
+        public IEnumerable<Client> GetAllClients()
+        {
+            return clientServiceDapper.GetAllClients();
+        }
+        [HttpGet]
+        public Client GetClientById(int id) 
+        {
+            return clientServiceDapper.GetById(id);
+
+        }
+        [HttpPost]
+        public IActionResult AddClient(Client client)
+        {
+            clientServiceDapper.AddClient(client);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult EditClient(Client client)
+        {
+            clientServiceDapper.Update(client);
+            return Ok(client);
+        }
+        [HttpDelete]
+        public void DeleteClient(int clientId)
+        {
+            clientServiceDapper.DeleteClient(clientId);
+        }
+    }
+}
---
+++ WebAPI_EF_Dapper/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using DapperAndEFcore.Models;
+using DapperAndEFcore.Service.EFcore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_EF_Dapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private ProductService productService;
+        public ProductController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var products = this.productService.GetAllProducts();
+            return products;
+
+        }
+    }
+}
